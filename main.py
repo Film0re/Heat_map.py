a2805f0a27a0b03dc119171c825775fff2503c90
Daniel <dandoodle956@gmail.com>
2022-07-31 00:42:14 -0500

    Fixed the centering on the graph and utilized some matplotlib functions on the website, heatmap and annotate_heatmap.

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib
+MAX_SIZE = 40
 
 def main():
-    array = load_array()
-    print_2d_array(array)
+    array, name = load_array()
+    # print_2d_array(array)
 
     X = np.array(array)
 
-    X_flipped = np.flipud(X)  # Flip array so that the heatmap matches the array
+    if (len(array)) < MAX_SIZE:
+        fig, ax = plt.subplots()
+    else:
+        fig, ax = plt.subplots(figsize=(20, 20))
 
-    print(X)
+    im, cbar = heatmap(X, cmap="bone_r")
 
-    # fig = plt.figure(figsize=(8, 6))
-    fig, ax = plt.subplots()
+    if (len(array)) < MAX_SIZE:
+        texts = annotate_heatmap(im, valfmt="{x:.1f}")
+    fig.tight_layout()
 
-    for i in range(5):
-        for j in range(5):
-            text = ax.text(j, i, X_flipped[i, j],
-                           ha="left", va="bottom", color="black")
-
-    plt.pcolormesh(X_flipped, cmap="pink_r")
-    plt.title("Plot 2D array")
-    plt.colorbar()
-    plt.savefig('fp.png')
+    plt.gcf()
+    plt.savefig(name + ".pdf")
     plt.show()
 
 
@@ -42,10 +40,13 @@ def load_array():
     Loads a 2d array of ints and returns it
     :return: A 2d array containing ints
     """
-    name = "p081_matrix.txt"
-    name2 = "5x5.txt"
+    five_by_five = "5x5.txt"
+    eighty_by_eighty = "p081_matrix.txt"
 
-    file = open(name2)
+    name = five_by_five
+    # name = eighty_by_eighty
+
+    file = open(name)
 
     file_lines = file.readlines()
     # print(file_lines)
@@ -59,7 +60,123 @@ def load_array():
 
     array2D = [list(map(int, i)) for i in array2D]
 
-    return array2D
+    return array2D, name
+
+
+def heatmap(data, row_labels=None, col_labels=None, ax=None,
+            cbar_kw={}, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (M, N).
+    row_labels
+        A list or array of length M with the labels for the rows.
+    col_labels
+        A list or array of length N with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # Show all ticks and label them with the respective list entries.
+    ax.set_xticks(np.arange(data.shape[1]), labels=col_labels)
+    ax.set_yticks(np.arange(data.shape[0]), labels=row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1] + 1) - .5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0] + 1) - .5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=("black", "white"),
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max()) / 2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
 
 
 # Press the green button in the gutter to run the script.
